35f8c0ce4c28c2c1386de06ea6098f0c3123205a
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-04-09 09:24:06 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,45 @@
 import React, { useState, useEffect } from "react";
 
-function UserTable() {}
+function UserTable() {
+    const[users,setUsers] = useState([]);
+    useEffect(()=>{
+        const getApiData = async () => {
+            const response = await fetch(
+              "https://jsonplaceholder.typicode.com/users"
+            ).then((response) => response.json());
+          
+            setUsers(response);
+          };
+          getApiData();
+    },[]);
+    
+      return(
+        <table>
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                    <td>Website</td>
+
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(user=>{
+                    return(
+                        <tr>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>   
+                            <td>{user.phone}</td>
+                            <td>{user.website}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+      )
+}
 
 export default UserTable;
